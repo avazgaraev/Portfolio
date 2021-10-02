5721c44ec22b9b580f8c601da3aed6d214d0f233
avazgaraev <83712949+avazgaraev@users.noreply.github.com>
2021-10-02 16:02:30 +0400

    Portfolio files

---
+++ Controllers/homeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication17.Models;
+
+namespace WebApplication17.Controllers
+{
+    public class homeController : Controller
+    {
+        dtDataContext db = new dtDataContext();
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult indexmain()
+        {
+            return View(db.cardds.ToList()); 
+        }
+        public ActionResult admin()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult adduser(cardd d)
+        {
+            db.cardds.InsertOnSubmit(d);
+            db.SubmitChanges();
+            return RedirectToAction("indexmain");
+        }
+        
+    }
+}
